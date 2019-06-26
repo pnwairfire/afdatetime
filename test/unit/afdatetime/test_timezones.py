8e9cfd13df80aca4ef123f11ebc1f6e809465ae8
sillson <sillson88@gmail.com>
2019-06-26 12:11:47 -0700

    dst

--- test/unit/afdatetime/test_timezones.py
+++ test/unit/afdatetime/test_timezones.py
@@ -7,6 +7,7 @@ import datetime
 from py.test import raises
 
 from afdatetime.timezones import UtcOffsetFinder
+from afdatetime.timezones import DstAccurateTimeZone
 
 
 class TestUtcOffsetFinder(object):
@@ -22,3 +23,19 @@ class TestUtcOffsetFinder(object):
             datetime.datetime(2015, 8, 1, 10, 9, 8))
         expected = (datetime.timedelta(hours=-7), '-07:00')
         assert expected == actual
+
+class TestDstAccurateTimeZone(object):
+
+    def test_pst(self):
+        actual = DstAccurateTimeZone().find(45.0, -118.4,
+            datetime.datetime(2015, 2, 1, 10, 9, 8))
+        expected = {'name': 'America/Los_Angeles', 'abbreviation': 'PST', 'dst': False}
+        assert expected == actual
+
+    def test_pdt(self):
+        actual = DstAccurateTimeZone().find(45.0, -118.4,
+            datetime.datetime(2015, 8, 1, 10, 9, 8))
+        expected = {'name': 'America/Los_Angeles', 'abbreviation': 'PDT', 'dst': True}
+        assert expected == actual
+
+
